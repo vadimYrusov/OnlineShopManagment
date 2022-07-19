eac35e4792fed27c1b02e9f8aa71d5efb20aff36
vadimYrusov <yurusovvadim845@gmail.com>
2022-07-19 23:53:43 +0300

    templates and controller

--- src/main/java/com/example/Online/shop/managment/controller/ItemController.java
+++ src/main/java/com/example/Online/shop/managment/controller/ItemController.java
@@ -5,8 +5,12 @@ import com.example.Online.shop.managment.repo.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import java.lang.reflect.Member;
 import java.util.List;
 
 @Controller
@@ -22,5 +26,21 @@ public class ItemController {
 
         model.addAttribute("items", items);
         return "items";
+    }
+
+    @GetMapping("/items/new")
+    public String createMember(Model model) {
+        ShopItem item = new ShopItem();
+        model.addAttribute("item", item);
+        return "create_item";
+    }
+
+    @PostMapping("/items")
+    public String saveMember(@ModelAttribute("item") ShopItem shopItem, BindingResult result) {
+        if (result.hasErrors()) {
+            return "create_item";
+        }
+        itemRepository.save(shopItem);
+        return "redirect:/items";
     }
 }
